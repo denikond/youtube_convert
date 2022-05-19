cdc56eb63302e959bb4dc513b20e01f2af670269
Denis Kondrashov <dkond@mail.ru>
2022-05-19 23:56:13 +0300

    Login form 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SECRET_KEY'] = '3JfEddpoWwHY'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from app import app
-from flask import  render_template
+from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'fox'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
