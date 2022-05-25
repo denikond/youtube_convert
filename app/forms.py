2f3755db31f1776a9648cb57ba6d172e9f0947a2
Denis Kondrashov <dkond@mail.ru>
2022-05-26 00:31:03 +0300

    date time input boxes in forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, DateField, TimeField
+#from wtforms.fields import DateField
 from wtforms.validators import DataRequired
+
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,13 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class LookingFor(FlaskForm):
+    #startdate = DateField('Начало', format='%d-%m-%Y', validators=[DataRequired(),])
+    #enddate = DateField('Конец', format='%d-%m-%Y', validators=[DataRequired(),])
+    startdate = DateField('Начало', format='%Y-%m-%d')
+    starttime = TimeField('stt', format="%H:%M")
+    enddate = DateField('Конец', format='%Y-%m-%d')
+    endtime = TimeField('ent', format="%H:%M")
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, session, request
+from app.forms import LoginForm, LookingFor
 
 
 @app.route('/')
@@ -18,3 +18,22 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/find', methods=['GET', 'POST'])
+def find():
+    form = LookingFor()
+    if form.validate_on_submit():
+        session['startdate'] = form.startdate.data
+        session['enddate'] = form.enddate.data
+        session['starttime'] = form.starttime.data
+        session['endtime'] = form.endtime.data
+        return redirect(url_for('date'))
+    return render_template('find.html', form=form)
+
+@app.route('/date', methods=['GET','POST'])
+def date():
+    startdate = session['startdate']
+    enddate = session['enddate']
+    starttime = session['starttime']
+    enddate = session['endtime']
+    return render_template('date.html')
